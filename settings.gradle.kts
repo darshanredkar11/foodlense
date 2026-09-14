91022923f622f55133596d1c3f235388e5a036d6
darshanredkar11 <darshan.redkar11@outlook.com>
2026-09-14 19:29:58 +0530

    build: add monorepo Gradle settings

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "FoodLense"
+
+include(":apps:android")
+include(":shared:domain")
+include(":shared:contracts")
